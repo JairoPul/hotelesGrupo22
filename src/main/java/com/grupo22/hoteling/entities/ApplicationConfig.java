b790b60afbb5e0fab8555c6a258828650f2a8709
JairoPul <jairo.pulido@alumnos.uva.es>
2022-05-24 18:52:09 +0200

    Initial commit

---
+++ src/main/java/com/grupo22/hoteling/entities/ApplicationConfig.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo22.hoteling.entities;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Jairo
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.grupo22.hoteling.entities.GenericResource.class);
+        resources.add(com.grupo22.hoteling.rest.HotelFacadeREST.class);
+        resources.add(com.grupo22.hoteling.rest.ReserveFacadeREST.class);
+        resources.add(com.grupo22.hoteling.rest.UserGroupsFacadeREST.class);
+        resources.add(com.grupo22.hoteling.rest.UserrelationFacadeREST.class);
+        resources.add(com.grupo22.hoteling.rest.UsersFacadeREST.class);
+    }
+    
+}
